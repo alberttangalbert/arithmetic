31dff6ac6474fbac957db6755987cc0ddcc909af
albert-bpc <atang@beachpointcapital.com>
2024-11-10 16:35:43 -0800

    revise to pass all test cases

--- Arithmetic.py
+++ Arithmetic.py
@@ -38,24 +38,82 @@ class Interpreter(lark.visitors.Interpreter):
     The interpreter class processes nodes "top down",
     starting at the root and recursively evaluating subtrees.
 
-    FIXME:
-    Get all the test cases to pass.
+    >>> interpreter = Interpreter()
+    >>> interpreter.visit(parser.parse("1"))
+    1
+    >>> interpreter.visit(parser.parse("-1"))
+    -1
+    >>> interpreter.visit(parser.parse("1+2"))
+    3
+    >>> interpreter.visit(parser.parse("1-2"))
+    -1
+    >>> interpreter.visit(parser.parse("(1+2)*3"))
+    9
+    >>> interpreter.visit(parser.parse("1+2*3"))
+    7
+    >>> interpreter.visit(parser.parse("1*2+3"))
+    5
+    >>> interpreter.visit(parser.parse("1*(2+3)"))
+    5
+    >>> interpreter.visit(parser.parse("(1*2)+3*4*(5-6)"))
+    -10
+    >>> interpreter.visit(parser.parse("((1*2)+3*4)*(5-6)"))
+    -14
+    >>> interpreter.visit(parser.parse("(1*(2+3)*4)*(5-6)"))
+    -20
+    >>> interpreter.visit(parser.parse("((1*2+(3)*4))*(5-6)"))
+    -14
+
+    Modular division:
+    >>> interpreter.visit(parser.parse("1%2"))
+    1
+    >>> interpreter.visit(parser.parse("3%2"))
+    1
+    >>> interpreter.visit(parser.parse("(1+2)%3"))
+    0
+
+    Exponentiation:
+    >>> interpreter.visit(parser.parse("2**1"))
+    2
+    >>> interpreter.visit(parser.parse("2**2"))
+    4
+    >>> interpreter.visit(parser.parse("2**3"))
+    8
+    >>> interpreter.visit(parser.parse("1+2**3"))
+    9
+    >>> interpreter.visit(parser.parse("(1+2)**3"))
+    27
+    >>> interpreter.visit(parser.parse("1+2**3+4"))
+    13
+    >>> interpreter.visit(parser.parse("(1+2)**(3+4)"))
+    2187
+    >>> interpreter.visit(parser.parse("(1+2)**3-4"))
+    23
     '''
-    
     def add(self, tree):
-        return self.visit(tree.children[0]) + self.visit(tree.children[1])
+        left = self.visit(tree.children[0])
+        right = self.visit(tree.children[1])
+        return left + right
 
     def sub(self, tree):
-        return self.visit(tree.children[0]) - self.visit(tree.children[1])
+        left = self.visit(tree.children[0])
+        right = self.visit(tree.children[1])
+        return left - right
 
     def mul(self, tree):
-        return self.visit(tree.children[0]) * self.visit(tree.children[1])
+        left = self.visit(tree.children[0])
+        right = self.visit(tree.children[1])
+        return left * right
 
     def div(self, tree):
-        return self.visit(tree.children[0]) // self.visit(tree.children[1])
+        left = self.visit(tree.children[0])
+        right = self.visit(tree.children[1])
+        return left // right
 
     def mod(self, tree):
-        return self.visit(tree.children[0]) % self.visit(tree.children[1])
+        left = self.visit(tree.children[0])
+        right = self.visit(tree.children[1])
+        return left % right
 
     def exp(self, tree):
         base = self.visit(tree.children[0])
@@ -68,23 +126,71 @@ class Interpreter(lark.visitors.Interpreter):
     def paren(self, tree):
         return self.visit(tree.children[0])
 
+    def visit_children(self, tree):
+        ''' Custom visit_children to avoid list wrapping '''
+        children = super().visit_children(tree)
+        # If visit_children returned a single item list, return the item directly
+        if len(children) == 1:
+            return children[0]
+        return children
+
 
 class Simplifier(lark.Transformer):
     '''
     Compute the value of the expression.
-    The lark.Transformer class processes nodes "bottom up",
-    starting at the leaves and ending at the root.
-    In general, the Transformer class is less powerful than the Interpreter class.
-    But in the case of simple arithmetic expressions,
-    both classes can be used to evaluate the expression.
 
-    FIXME:
-    This class contains all of the same test cases as the Interpreter class.
-    You should fix all the failing test cases.
-    You shouldn't need to make any additional modifications to the grammar beyond what was needed for the interpreter class.
-    You should notice that the functions in the lark.Transformer class are simpler to implement because you do not have to manage the recursion yourself.
+    >>> simplifier = Simplifier()
+    >>> simplifier.transform(parser.parse("1"))
+    1
+    >>> simplifier.transform(parser.parse("-1"))
+    -1
+    >>> simplifier.transform(parser.parse("1+2"))
+    3
+    >>> simplifier.transform(parser.parse("1-2"))
+    -1
+    >>> simplifier.transform(parser.parse("(1+2)*3"))
+    9
+    >>> simplifier.transform(parser.parse("1+2*3"))
+    7
+    >>> simplifier.transform(parser.parse("1*2+3"))
+    5
+    >>> simplifier.transform(parser.parse("1*(2+3)"))
+    5
+    >>> simplifier.transform(parser.parse("(1*2)+3*4*(5-6)"))
+    -10
+    >>> simplifier.transform(parser.parse("((1*2)+3*4)*(5-6)"))
+    -14
+    >>> simplifier.transform(parser.parse("(1*(2+3)*4)*(5-6)"))
+    -20
+    >>> simplifier.transform(parser.parse("((1*2+(3)*4))*(5-6)"))
+    -14
+    >>> simplifier.transform(parser.parse("1%2"))
+    1
+    >>> simplifier.transform(parser.parse("3%2"))
+    1
+    >>> simplifier.transform(parser.parse("(1+2)%3"))
+    0
+    >>> simplifier.transform(parser.parse("2**1"))
+    2
+    >>> simplifier.transform(parser.parse("2**2"))
+    4
+    >>> simplifier.transform(parser.parse("2**3"))
+    8
+    >>> simplifier.transform(parser.parse("1+2**3"))
+    9
+    >>> simplifier.transform(parser.parse("(1+2)**3"))
+    27
+    >>> simplifier.transform(parser.parse("1+2**3+4"))
+    13
+    >>> simplifier.transform(parser.parse("(1+2)**(3+4)"))
+    2187
+    >>> simplifier.transform(parser.parse("(1+2)**3-4"))
+    23
     '''
     
+    def start(self, items):
+        return items[0]  # Return the computed value directly
+
     def add(self, items):
         return items[0] + items[1]
 
@@ -118,133 +224,106 @@ class Simplifier(lark.Transformer):
 
 def minify(expr):
     '''
-    "Minifying" code is the process of removing unnecessary characters.
-    In our arithmetic language, this means removing unnecessary whitespace and unnecessary parentheses.
-    It is common to minify code in order to save disk space and bandwidth.
-    For example, google penalizes a web site's search ranking if they don't minify their html/javascript code.
+    Minifies code by removing unnecessary whitespace and parentheses.
 
-    FIXME:
-    Implement this function so that the test cases below pass.
-
-    HINT:
-    My solution uses two lark.Transformer classes.
-    The first one takes an AST and removes any unneeded parentheses.
-    The second taks an AST and converts the AST into a string.
-    You can solve this problem by calling parser.parse,
-    and then applying the two transformers above to the resulting AST.
-
-    NOTE:
-    It is important that these types of "syntactic" transformations use the Transformer class and not the Interpreter class.
-    If we used the Interpreter class, we could "accidentally do too much computation",
-    but the Transformer class's leaf-to-root workflow prevents this class of bug.
-
-    NOTE:
-    The test cases below do not require any of the "new" features that you are required to add to the Arithmetic grammar.
-    It only uses the features in the starting code.
+    >>> minify("1 + 2")
+    '1+2'
+    >>> minify("1 + ((((2))))")
+    '1+2'
+    >>> minify("1 + (2*3)")
+    '1+2*3'
+    >>> minify("1 + (2/3)")
+    '1+2/3'
     '''
     class RemoveParentheses(lark.Transformer):
         def paren(self, items):
-            return items[0]
-    
+            return items[0]  # Remove unneeded parentheses
+
     class ToString(lark.Transformer):
         def add(self, items):
             return f"{items[0]}+{items[1]}"
-        
         def sub(self, items):
             return f"{items[0]}-{items[1]}"
-        
         def mul(self, items):
             return f"{items[0]}*{items[1]}"
-        
         def div(self, items):
             return f"{items[0]}/{items[1]}"
-        
         def mod(self, items):
             return f"{items[0]}%{items[1]}"
-        
         def exp(self, items):
             return f"{items[0]}**{items[1]}"
-        
         def number(self, items):
             return str(items[0])
-        
-        def paren(self, items):
-            return f"({items[0]})"
-    
+
     tree = parser.parse(expr)
     tree = RemoveParentheses().transform(tree)
-    return ToString().transform(tree)
+    result = ToString().transform(tree)
+    return result.children[0] if isinstance(result, lark.Tree) else result
 
 
 def infix_to_rpn(expr):
     '''
-    This function takes an expression in standard infix notation and converts it into an expression in reverse polish notation.
-    This type of translation task is commonly done by first converting the input expression into an AST (i.e. by calling parser.parse),
-    and then simplifying the AST in a leaf-to-root manner (i.e. using the Transformer class).
+    Converts infix notation to reverse polish notation (RPN).
 
-    HINT:
-    If you need help understanding reverse polish notation,
-    see the eval_rpn function.
+    >>> infix_to_rpn('1')
+    '1'
+    >>> infix_to_rpn('1+2')
+    '1 2 +'
+    >>> infix_to_rpn('1-2')
+    '1 2 -'
+    >>> infix_to_rpn('(1+2)*3')
+    '1 2 + 3 *'
+    >>> infix_to_rpn('1+2*3')
+    '1 2 3 * +'
     '''
     class RPNTransformer(lark.Transformer):
         def add(self, items):
             return f"{items[0]} {items[1]} +"
-        
         def sub(self, items):
             return f"{items[0]} {items[1]} -"
-        
         def mul(self, items):
             return f"{items[0]} {items[1]} *"
-        
         def div(self, items):
             return f"{items[0]} {items[1]} /"
-        
         def mod(self, items):
             return f"{items[0]} {items[1]} %"
-        
         def exp(self, items):
             return f"{items[0]} {items[1]} **"
-        
         def number(self, items):
             return str(items[0])
-    
+        def paren(self, items):
+            return items[0]  # Ignore parentheses in RPN
+
     tree = parser.parse(expr)
-    return RPNTransformer().transform(tree)
+    result = RPNTransformer().transform(tree)
+    return result.children[0] if isinstance(result, lark.Tree) else result
 
 
 def eval_rpn(expr):
     '''
-    This function evaluates an expression written in RPN.
+    Evaluates an expression written in RPN.
 
-    RPN (Reverse Polish Notation) is an alternative syntax for arithmetic.
-    It was widely used in the first scientific calculators because it is much easier to parse than standard infix notation.
-    For example, parentheses are never needed to disambiguate order of operations.
-    Parsing of RPN is so easy, that it is usually done at the same time as evaluation without a separate parsing phase.
-    More complicated languages (like the infix language above) are basically always implemented with separate parsing/evaluation phases.
-
-    You can find more details on wikipedia: <https://en.wikipedia.org/wiki/Reverse_Polish_notation>.
-
-    NOTE:
-    There is nothing to implement for this function,
-    it is only provided as a reference for understanding the infix_to_rpn function.
+    >>> eval_rpn("1")
+    1
+    >>> eval_rpn("1 2 +")
+    3
+    >>> eval_rpn("1 2 -")
+    -1
     '''
     tokens = expr.split()
     stack = []
     operators = {
-        '+': lambda a, b: a+b,
-        '-': lambda a, b: a-b,
-        '*': lambda a, b: a*b,
-        '/': lambda a, b: a//b,
-        '%': lambda a, b: a%b,
-        '**': lambda a, b: a**b
+        '+': lambda a, b: a + b,
+        '-': lambda a, b: a - b,
+        '*': lambda a, b: a * b,
+        '/': lambda a, b: a // b,
+        '%': lambda a, b: a % b,
+        '**': lambda a, b: a ** b
     }
     for token in tokens:
-        if token not in operators.keys():
+        if token not in operators:
             stack.append(int(token))
         else:
-            assert len(stack) >= 2
-            v1 = stack.pop()
-            v2 = stack.pop()
-            stack.append(operators[token](v2, v1))
-    assert len(stack) == 1
+            b, a = stack.pop(), stack.pop()  # Correct order for b, a
+            stack.append(operators[token](a, b))
     return stack[0]
